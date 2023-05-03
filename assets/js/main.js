6b6f99b146b7f2e2a5c4ffcc055a9f78b043c8d3
christopher-yi <christopher.yi319@gmail.com>
2023-05-02 22:26:02 -0400

    Add autocomplete functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -223,6 +223,8 @@ function getCocktail() {
 	if(drink == '') {
 		return [];
 	}
+	
+	document.querySelector(".popup-list").innerHTML = '';
 
 	//*******/ If input is not an exact match it will not return the drink, if the input does not match strDrink, defer to select autocomplete focus drink instead********
 
@@ -235,17 +237,52 @@ function getCocktail() {
 			document.querySelector('.popup-image').src = data.drinks[0].strDrinkThumb
 			document.querySelector('.popup-image').style.width = '20rem'
 			document.querySelector('.popup-instructions').innerHTML = data.drinks[0].strInstructions;
+
+			for(let key in data.drinks[0]) {
+				if(key.includes('strIngredient') && data.drinks[0][key] !== null) {
+					ingredientsArr.push(data.drinks[0][key])
+				}
+			  }
+		
+			  for(let key in data.drinks[0]) {
+				if(key.includes('strMeasure') && data.drinks[0][key] !== null) {
+					measurementsArr.push(data.drinks[0][key])
+				}
+			  }
+		
+			  for(let i = 0; i < measurementsArr.length; i++) {
+				for(let j = 0; j < ingredientsArr.length; j++) {
+					if(i == j) {
+						ingredientsArr[j] += ': ' + measurementsArr[i]
+					}
+				}
+			  }
+			  
+			  console.log(ingredientsArr)
+		
+			  ingredientsArr.forEach(step => {
+				const instruct = `<li>${step}<li>`;
+				document.querySelector('.popup-list').insertAdjacentHTML('beforeend', instruct);
+			  })
+		
+			  document.querySelector('.popup-list').style.display = 'inline-block'
+			  document.querySelector('.popup-list').style.listStyle = 'none'
+		
+			  ingredientsArr = [];
+	  
+
 		} else {
-			// Make a autocomplete div that says 'no results named `${drink}` found
+			// Make a autocomplete div that says 'no results named `${drink}` found - Make after autocomplete feature
 		}
 	}
 
 		console.log(drinkArray)
-
+	ingredientsArr = [];
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
+	return ingredientsArr
 }
 
 let ingredientName = [];
@@ -259,6 +296,7 @@ function listCocktails() {
     .then(res => res.json()) // parse response as JSON
     .then(data => {
 
+		drinkName = []
 		console.log(data.drinks)
 
 		for(let key in data.drinks) {
@@ -284,8 +322,9 @@ function listCocktails() {
 		}
 
 		console.log([...new Set(drinkName.concat(ingredientName))])
+		
+		autocomplete(document.getElementById("myInput"), drinkName);
 
-		// Add autocomplete here -.- using drinkName array
 
     })
     .catch(err => {
@@ -317,7 +356,7 @@ function getRandom() {
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data.drinks);
-      
+      drinkName = []
 	  document.querySelector('.lucky').style.display = 'none'
 	  document.querySelector('.lucky2').style.display = 'none'
 	  
@@ -369,20 +408,104 @@ function getRandom() {
 }
 
 
-// Autocomplete
-
 // Autocomplete feature https://www.w3schools.com/howto/howto_js_autocomplete.asp
 
-// ->>> While the search bar contains the characters the user puts in, create an alphabetically sorted array of the drinks that start with those characters and then create a list and make them appear on drop down list
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// Separate the listCocktail and getCocktail functions
-
-// listCocktails will fire when the 'input' event listener on the text input fires
-// it will also create an autocomplete list on each input change
-// need to also implement focus, and keyup, keydown on list
-
-// getCocktail will fire only when enter is pressed on text input or when submit button is pressed, get cocktail will change the popup container and add drink data onto it.
-
-
 // For featured recipes return drinks from a random ingredient list on page load???
+
+// Autocomplete function
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+
+autocomplete(document.getElementById("myInput"), drinkName);
